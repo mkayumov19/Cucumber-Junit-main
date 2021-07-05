18d283b5c7d901a5acdd56b05890b625727d498a
Makhmudjon <makhmudik19@gmail.com>
2021-07-05 18:48:21 -0400

    Initial commit

---
+++ src/test/java/com/cybertek/runners/HelloJava.java
@@ -0,0 +1,7 @@
+package com.cybertek.runners;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+    }
+}
---
+++ src/test/java/com/cybertek/utilities/BrowserUtils.java
@@ -0,0 +1,18 @@
+package com.cybertek.utilities;
+
+public class BrowserUtils {
+    /**
+     * Method that will accept int
+     * Wait for given second duration
+     */
+
+    public static void sleep(int second) {
+        second *= 1000;         // 1 second = 1000 milliseconds
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+            System.out.println("Something happened in the Sleep Method");
+        }
+
+    }
+}
