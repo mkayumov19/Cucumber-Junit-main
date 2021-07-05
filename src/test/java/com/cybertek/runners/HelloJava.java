c370e7dbd0e9a42f371c8f1fa6452adb7c7c98f9
Makhmudjon <makhmudik19@gmail.com>
2021-07-05 19:34:37 -0400

    Created a branch TC1Cucumber-2

--- src/test/java/com/cybertek/runners/HelloJava.java
+++ src/test/java/com/cybertek/runners/HelloJava.java
@@ -4,5 +4,6 @@ public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello java");
         System.out.println("Hello java1");
+        System.out.println("Hello java2");
     }
 }
