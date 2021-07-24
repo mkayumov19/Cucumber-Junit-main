0ed8ba5ff40c6b74530e6f27c973377f9f31b0b1
Makhmudjon <makhmudik19@gmail.com>
2021-07-24 11:59:04 -0400

    new push

--- src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
@@ -1,45 +1,66 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.pages.DropdownsPage;
 import com.cybertek.pages.LibraryLoginPage;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class DataTables_StepDefinitions {
     LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
-
-    @Then("user should see below words displayed")
-    public void user_should_see_below_words_displayed(List<String> listOfFruits) {
-        System.out.println("listOfFruits.size() = " + listOfFruits.size());
-        System.out.println("listOfFruits = " + listOfFruits);
-
-    }
+    DropdownsPage dropdownsPage = new DropdownsPage();
 
     @Given("User is on the login page of library app")
     public void user_is_on_the_login_page_of_library_app() {
         String url = ConfigurationReader.getProperty("url");
         Driver.getDriver().get(url);
-
     }
 
     @When("User enters username and password as below")
     public void user_enters_username_and_password_as_below(Map<String, String> loginInfo) {
         String username = loginInfo.get("username");
 //        String password = loginInfo.get("password");
-
         libraryLoginPage.emailInput.sendKeys(username);
         libraryLoginPage.passwordInput.sendKeys(loginInfo.get("password"));
         libraryLoginPage.signIn.click();
-
     }
 
     @Then("User should see title is Library")
     public void userShouldSeeTitleIsLibrary() {
         System.out.println("Title verification");
     }
+
+    @Then("user should see below words displayed")
+    public void user_should_see_below_words_displayed(List<String> listOfFruits) {
+        System.out.println("listOfFruits.size() = " + listOfFruits.size());
+        System.out.println("listOfFruits = " + listOfFruits);
+    }
+
+    @Given("User is on the dropdowns page of practice tool")
+    public void user_is_on_the_dropdowns_page_of_practice_tool() {
+        Driver.getDriver().get("http://practice.cybertekschool.com/dropdown");
+    }
+
+    @Then("User should see below info in month dropdown")
+    public void user_should_see_below_info_in_month_dropdown(List<String> expectedList) {
+        Select monthDropdown = new Select(dropdownsPage.month);
+        List<WebElement> actualMonthWebElement = monthDropdown.getOptions();
+        List<String> actualMonthAsString = new ArrayList<>();
+        for (WebElement each : actualMonthWebElement) {
+            actualMonthAsString.add(each.getText());
+        }
+        Assert.assertEquals(expectedList, actualMonthAsString);
+        System.out.println("Expected months = "+expectedList);
+        System.out.println("Actual months = "+actualMonthAsString);
+    }
+
 }
--- src/test/resources/features/dataTable.feature
+++ src/test/resources/features/dataTable.feature
@@ -1,24 +1,42 @@
 #  @wip
-  Feature: Examples of Cucumber data table implementations
+Feature: Examples of Cucumber data table implementations
 
-    Scenario: Something happens
+  @wip
+  Scenario: User should be able to see all 12 months in months dropdown
+    Given User is on the dropdowns page of practice tool
+    Then User should see below info in month dropdown
+      | January   |
+      | February  |
+      | March     |
+      | April     |
+      | May       |
+      | June      |
+      | July      |
+      | August    |
+      | September |
+      | October   |
+      | November  |
+      | December  |
+
+  Scenario: Something happens
     #Given user does something
-      Then user should see below words displayed
-        | Apple      |
-        | Kiwi       |
-        | Orange     |
-        | Strawberry |
-        | Mango      |
-        | Tomato     |
-        | Cucumber   |
+    Then user should see below words displayed
+      | Apple      |
+      | Kiwi       |
+      | Orange     |
+      | Strawberry |
+      | Mango      |
+      | Tomato     |
+      | Cucumber   |
 
     # To fix pipes' order
       #MAC: command + option + L
       #Windows : control + alt + L
-    @wip
-    Scenario: Library login example using data table
-      Given User is on the login page of library app
-      When User enters username and password as below
-      |username|student11@library|
-      |password|tScBPCUr         |
-      Then User should see title is Library
+
+#  @wip
+  Scenario: Library login example using data table
+    Given User is on the login page of library app
+    When User enters username and password as below
+      | username | student11@library |
+      | password | tScBPCUr          |
+    Then User should see title is Library
